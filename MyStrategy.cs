0b25b34316f1ccea189ee52d936b0bcf137244b0
Eugenie <zn@bk.ru>
2015-11-17 23:25:05 +1000

    Update MyStrategy.cs

--- MyStrategy.cs
+++ MyStrategy.cs
@@ -11,6 +11,50 @@ namespace Com.CodeGame.CodeRacing2015.DevKit.CSharpCgdk {
             if (world.Tick > game.InitialFreezeDurationTicks) {
                 move.IsUseNitro = true;
             }
+
+            double nextWaypointX = (self.NextWaypointX + 0.5D) * game.TrackTileSize;
+            double nextWaypointY = (self.NextWaypointY + 0.5D) * game.TrackTileSize;
+
+            double cornerTileOffset = 0.25D * game.TrackTileSize;
+
+            switch (world.TilesXY[self.NextWaypointX][self.NextWaypointY])
+            {
+                case TileType.LeftTopCorner:
+                    nextWaypointX += cornerTileOffset;
+                    nextWaypointY += cornerTileOffset;
+                    break;
+                case TileType.RightTopCorner:
+                    nextWaypointX -= cornerTileOffset;
+                    nextWaypointY += cornerTileOffset;
+                    break;
+                case TileType.LeftBottomCorner:
+                    nextWaypointX += cornerTileOffset;
+                    nextWaypointY -= cornerTileOffset;
+                    break;
+                case TileType.RightBottomCorner:
+                    nextWaypointX -= cornerTileOffset;
+                    nextWaypointY -= cornerTileOffset;
+                    break;
+                default:
+                    break;
+            }
+
+            double angleToWaypoint = self.GetAngleTo(nextWaypointX, nextWaypointY);
+            double speedModule = Hypot(self.SpeedX, self.SpeedY);
+            
+            move.WheelTurn = angleToWaypoint * 32.0D / Math.PI;
+            move.EnginePower = (0.75D);
+
+            if (speedModule * speedModule * Math.Abs(angleToWaypoint) > 2.5D * 2.5D * Math.PI)
+            {
+                move.IsBrake = true;
+            }
+
         }
+
+        double Hypot(double x, double y) { return Math.Sqrt(x * x + y * y); }
+
     }
+
+
 }
