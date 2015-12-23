76cdc20fe115cdcf18c7735f2f5826df561d9a45
znsoft <zn@bk.ru>
2015-12-23 23:17:33 +1000

    оработка Вестор2 на следующий год

--- MyStrategy.cs
+++ MyStrategy.cs
@@ -1165,6 +1165,25 @@ namespace Com.CodeGame.CodeRacing2015.DevKit.CSharpCgdk
                 return (v1.x * v2.y) - (v1.y * v2.x);
             }
 
+            public static Vector2 operator +(Vector2 v1, Vector2 v2)
+            {
+                return new Vector2(v1.x + v2.x, v1.y + v2.y);
+            }
+            public static Vector2 operator -(Vector2 v1, Vector2 v2)
+            {
+                return new Vector2(v1.x - v2.x, v1.y - v2.y);
+            }
+
+            public static Vector2 operator *(Vector2 v, double r)
+            {
+                return new Vector2(v.x * r, v.y * r);
+            }
+
+            public static double operator *(Vector2 v1, Vector2 v2)
+            {
+                return v1.x * v2.x + v1.y * v2.y;
+            }
+
         }
 
         public class WaveMapCell// клетка в формате волнового алгоритма
